f81a2d4bd9bfc0ce6a69ac1e11df088a25a1570d
Daniel Renaldo G Santana <149911858+Sandarg95@users.noreply.github.com>
2024-09-29 06:27:58 -0400

    Add files via upload

---
+++ models/course.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const courseSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,  // Removes any extra spaces
+        minlength: 3 // Minimum length validation
+    },
+    subject: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    numberOfCredits: {
+        type: Number,
+        required: true,
+        min: 1 // Ensures the number of credits is at least 1
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: function (value) {
+                return value > this.startDate;
+            },
+            message: "End date must be after the start date."
+        }
+    },
+    teacher: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, { timestamps: true });
+
+const Course = mongoose.model('Course', courseSchema);
+module.exports = Course;
